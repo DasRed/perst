b5781eb28b1637fea984ec5f3f0bbdc02572bf1b
Marco Starker <marco.starker@gmx.net>
2020-12-07 15:57:20 +0100

    test: adding more tests

---
+++ src/config/__tests__/schema.test.js
@@ -0,0 +1,433 @@
+import schema from '../schema.js';
+import unknownPropertyObject from '../unknownPropertyObject.js';
+
+describe('schema.js', () => {
+    test('with defaults', () => {
+        const result = schema({});
+
+        expect(result).toEqual({
+            $$strict: 'remove',
+            dryRun:   {
+                type:     'boolean',
+                required: true,
+                default:  false,
+            },
+            ci:       {
+                type:     'boolean',
+                required: true,
+                default:  false,
+            },
+            api:      {
+                type:   'object',
+                strict: 'remove',
+                props:  {
+                    token:   {
+                        type:     'string',
+                        required: true,
+                        pattern:  /^[A-fa-f0-9]{32}$/gi,
+                    },
+                    server:  {
+                        type:     'url',
+                        required: true,
+                        default:  'https://api.loader.io',
+                    },
+                    version: {
+                        type:     'enum',
+                        values:   ['v2'],
+                        required: true,
+                        default:  'v2',
+                    }
+                }
+            },
+            app:      {
+                type:   'object',
+                strict: 'remove',
+                props:  {
+                    domain: {
+                        type:     'url',
+                        required: true,
+                    }
+                }
+            },
+            tasks:    {
+                type:     'custom',
+                check:    unknownPropertyObject,
+                minProps: 1,
+                items:    {
+                    name:           {
+                        type:     'string',
+                        required: true,
+                    },
+                    type:           {
+                        type:     'enum',
+                        values:   ['per-test', 'per-second', 'maintain-load'],
+                        required: true,
+                        default:  'per-second',
+                    },
+                    duration:       {
+                        type:     'number',
+                        required: true,
+                        integer:  true,
+                        min:      60,
+                    },
+                    clientsStart:   {
+                        type:     'number',
+                        required: true,
+                        default:  1,
+                        integer:  true,
+                        min:      0,
+                    },
+                    clients:        {
+                        type:     'number',
+                        required: true,
+                        integer:  true,
+                        min:      15,
+                    },
+                    timeout:        {
+                        type:     'number',
+                        required: true,
+                        default:  10000,
+                        integer:  true,
+                        positive: true,
+                    },
+                    errorThreshold: {
+                        type:     'number',
+                        required: true,
+                        default:  50,
+                        integer:  true,
+                        min:      0,
+                        max:      100,
+                    },
+                    notes:          {
+                        type:     'string',
+                        optional: true,
+                    },
+                    tags:           {
+                        type:     'array',
+                        items:    'string',
+                        optional: true,
+                    },
+                    threshold:      {
+                        type:     'object',
+                        strict:   'remove',
+                        required: true,
+                        default:  {
+                            avgResponseTime: 500,
+                            avgErrorRate:    50
+                        },
+                        props:    {
+                            avgResponseTime: {
+                                type:     'number',
+                                required: true,
+                                default:  500,
+                                integer:  true,
+                                positive: true,
+                            },
+                            avgErrorRate:    {
+                                type:     'number',
+                                required: true,
+                                default:  50,
+                                min:      0,
+                                max:      100
+                            },
+                        },
+                    },
+                    request:        {
+                        type:   'object',
+                        strict: 'remove',
+                        props:  {
+                            path:           {
+                                type:     'string',
+                                required: true,
+                            },
+                            type:           {
+                                type:     'enum',
+                                values:   ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+                                required: true,
+                                default:  'GET',
+                            },
+                            payloadFile:    {
+                                type:     'url',
+                                optional: true,
+                            },
+                            headers:        {
+                                type:     'object',
+                                strict:   false,
+                                minProps: 1,
+                                optional: true,
+                                default:  {
+                                    'accept-encoding': 'gzip'
+                                }
+                            },
+                            parameters:     {
+                                type:     'object',
+                                strict:   false,
+                                minProps: 1,
+                                optional: true,
+                            },
+                            authentication: {
+                                type:     'object',
+                                optional: true,
+                                strict:   'remove',
+                                props:    {
+                                    type:     {
+                                        type:     'enum',
+                                        values:   ['basic'],
+                                        required: true,
+                                        default:  'basic',
+                                    },
+                                    login:    {
+                                        type:     'string',
+                                        required: true,
+                                    },
+                                    password: {
+                                        type:     'string',
+                                        required: true,
+                                    },
+                                },
+                            },
+                            variables:      {
+                                type:     'array',
+                                empty:    false,
+                                optional: true,
+                                items:    {
+                                    type:  'object',
+                                    props: {
+                                        name:     {
+                                            type:     'string',
+                                            required: true
+                                        },
+                                        property: {
+                                            type:     'string',
+                                            required: true
+                                        },
+                                        source:   {
+                                            type:     'string',
+                                            required: true
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                }
+            }
+        })
+    });
+
+    test('with cli values', () => {
+        const result = schema({
+            dryRun: true,
+            ci:     true
+        });
+
+        expect(result).toEqual({
+            $$strict: 'remove',
+            dryRun:   {
+                type:     'boolean',
+                required: true,
+                default:  true,
+            },
+            ci:       {
+                type:     'boolean',
+                required: true,
+                default:  true,
+            },
+            api:      {
+                type:   'object',
+                strict: 'remove',
+                props:  {
+                    token:   {
+                        type:     'string',
+                        required: true,
+                        pattern:  /^[A-fa-f0-9]{32}$/gi,
+                    },
+                    server:  {
+                        type:     'url',
+                        required: true,
+                        default:  'https://api.loader.io',
+                    },
+                    version: {
+                        type:     'enum',
+                        values:   ['v2'],
+                        required: true,
+                        default:  'v2',
+                    }
+                }
+            },
+            app:      {
+                type:   'object',
+                strict: 'remove',
+                props:  {
+                    domain: {
+                        type:     'url',
+                        required: true,
+                    }
+                }
+            },
+            tasks:    {
+                type:     'custom',
+                check:    unknownPropertyObject,
+                minProps: 1,
+                items:    {
+                    name:           {
+                        type:     'string',
+                        required: true,
+                    },
+                    type:           {
+                        type:     'enum',
+                        values:   ['per-test', 'per-second', 'maintain-load'],
+                        required: true,
+                        default:  'per-second',
+                    },
+                    duration:       {
+                        type:     'number',
+                        required: true,
+                        integer:  true,
+                        min:      60,
+                    },
+                    clientsStart:   {
+                        type:     'number',
+                        required: true,
+                        default:  1,
+                        integer:  true,
+                        min:      0,
+                    },
+                    clients:        {
+                        type:     'number',
+                        required: true,
+                        integer:  true,
+                        min:      15,
+                    },
+                    timeout:        {
+                        type:     'number',
+                        required: true,
+                        default:  10000,
+                        integer:  true,
+                        positive: true,
+                    },
+                    errorThreshold: {
+                        type:     'number',
+                        required: true,
+                        default:  50,
+                        integer:  true,
+                        min:      0,
+                        max:      100,
+                    },
+                    notes:          {
+                        type:     'string',
+                        optional: true,
+                    },
+                    tags:           {
+                        type:     'array',
+                        items:    'string',
+                        optional: true,
+                    },
+                    threshold:      {
+                        type:     'object',
+                        strict:   'remove',
+                        required: true,
+                        default:  {
+                            avgResponseTime: 500,
+                            avgErrorRate:    50
+                        },
+                        props:    {
+                            avgResponseTime: {
+                                type:     'number',
+                                required: true,
+                                default:  500,
+                                integer:  true,
+                                positive: true,
+                            },
+                            avgErrorRate:    {
+                                type:     'number',
+                                required: true,
+                                default:  50,
+                                min:      0,
+                                max:      100
+                            },
+                        },
+                    },
+                    request:        {
+                        type:   'object',
+                        strict: 'remove',
+                        props:  {
+                            path:           {
+                                type:     'string',
+                                required: true,
+                            },
+                            type:           {
+                                type:     'enum',
+                                values:   ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+                                required: true,
+                                default:  'GET',
+                            },
+                            payloadFile:    {
+                                type:     'url',
+                                optional: true,
+                            },
+                            headers:        {
+                                type:     'object',
+                                strict:   false,
+                                minProps: 1,
+                                optional: true,
+                                default:  {
+                                    'accept-encoding': 'gzip'
+                                }
+                            },
+                            parameters:     {
+                                type:     'object',
+                                strict:   false,
+                                minProps: 1,
+                                optional: true,
+                            },
+                            authentication: {
+                                type:     'object',
+                                optional: true,
+                                strict:   'remove',
+                                props:    {
+                                    type:     {
+                                        type:     'enum',
+                                        values:   ['basic'],
+                                        required: true,
+                                        default:  'basic',
+                                    },
+                                    login:    {
+                                        type:     'string',
+                                        required: true,
+                                    },
+                                    password: {
+                                        type:     'string',
+                                        required: true,
+                                    },
+                                },
+                            },
+                            variables:      {
+                                type:     'array',
+                                empty:    false,
+                                optional: true,
+                                items:    {
+                                    type:  'object',
+                                    props: {
+                                        name:     {
+                                            type:     'string',
+                                            required: true
+                                        },
+                                        property: {
+                                            type:     'string',
+                                            required: true
+                                        },
+                                        source:   {
+                                            type:     'string',
+                                            required: true
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                }
+            }
+        })
+    });
+});
---
+++ src/domain/__tests__/validate.test.js
@@ -0,0 +1,51 @@
+import Application from 'loader.io.api/dist/Application/Application.js';
+import LoaderIO from 'loader.io.api/dist/LoaderIO.js';
+import validate from '../validate.js';
+import logger from '../../logger.js';
+
+describe('validate.js', () => {
+    const loaderIO = new LoaderIO({token: 'a'});
+
+    describe('application found', () => {
+        test.each([
+            [Application.STATUS.VERIFIED, true],
+            [Application.STATUS.UNVERIFIED, false],
+        ])('application.status = %s', async (status, expected) => {
+            const applicationA = new Application({}, {app: 'www.nuff.de'});
+            const applicationB = new Application({}, {
+                app: 'www.narf.de',
+                status
+            });
+
+            const applicationsListSpy = jest.spyOn(loaderIO.applications, 'list').mockResolvedValue([applicationA, applicationB]);
+            const loggerLogSpy        = jest.spyOn(logger, 'log').mockReturnThis();
+
+            expect(await validate(loaderIO, {app: {domain: 'https://www.narf.de'}})).toBe(expected);
+            expect(applicationsListSpy).toHaveBeenCalled();
+            expect(loggerLogSpy).not.toHaveBeenCalled();
+        });
+    });
+
+    test('application not found', async () => {
+        const applicationA = new Application({}, {app: 'www.nuff.de'});
+        const applicationB = new Application({}, {app: 'www.narf.de'});
+
+        const applicationsListSpy = jest.spyOn(loaderIO.applications, 'list').mockResolvedValue([applicationA, applicationB]);
+        const loggerLogSpy        = jest.spyOn(logger, 'log').mockReturnThis();
+
+        expect(await validate(loaderIO, {app: {domain: 'https://www.rofl.de'}})).toBe(false);
+        expect(applicationsListSpy).toHaveBeenCalled();
+        expect(loggerLogSpy).not.toHaveBeenCalled();
+    });
+
+    test('load error', async () => {
+        const error = new Error();
+
+        const applicationsListSpy = jest.spyOn(loaderIO.applications, 'list').mockRejectedValue(error);
+        const loggerLogSpy        = jest.spyOn(logger, 'log').mockReturnThis();
+
+        expect(await validate(loaderIO, {app: {domain: 'https://www.rofl.de'}})).toBe(false);
+        expect(applicationsListSpy).toHaveBeenCalled();
+        expect(loggerLogSpy).toHaveBeenCalledWith(error);
+    });
+});
