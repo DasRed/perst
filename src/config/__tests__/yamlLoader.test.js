322ad214e9ab03e3d4fda0d8dc159ba20a9f5354
Marco Starker <marco.starker@gmx.net>
2020-12-09 09:48:26 +0100

    test: adding tests for yaml loader with env vars

---
+++ src/config/__tests__/yamlLoader.test.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import yamlLoader from '../yamlLoader.js';
+
+describe('yamlLoader.js', () => {
+    test('success', () => {
+        const file    = __dirname + '/fixture/yamlLoader.success.yml';
+        const content = fs.readFileSync(file, 'utf-8');
+        const result  = yamlLoader(file, content, {COPTER: 'nope'});
+
+        expect(result).toEqual({
+            nuff: 'narf',
+            rofl: 'nope',
+            lol:  'nope'
+        });
+    });
+
+    test('failed',  () => {
+        const file    = __dirname + '/fixture/yamlLoader.failed.yml';
+        const content = fs.readFileSync(file, 'utf-8');
+
+        expect( () => yamlLoader(file, content, {COPTER: 'nope'})).toThrowError(`YAML Error in ${file}:\nImplicit map keys need to be on a single line at line 1, column 1:\n\nnuff\n^^^^…\n`);
+    });
+});
