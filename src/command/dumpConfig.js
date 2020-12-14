2a36751751134f6caa091fdb2e26419f6b67f2c3
Sebastian Bartus-Kunz <sebastian.bartus-kunz@z-lab.com>
2020-12-14 11:33:29 +0100

    feat(dump-config): add syntax highlighting to config dump

--- src/command/dumpConfig.js
+++ src/command/dumpConfig.js
@@ -1,5 +1,6 @@
 import YAML from 'yaml';
 import logger from '../logger.js';
+import {highlight} from 'cli-highlight';
 
 /**
  *
@@ -11,18 +12,18 @@ export default async function dumpConfig(config) {
     delete config.dumpConfig;
 
     if (format === 'json') {
-        logger.log(JSON.stringify(config, null, 4));
+        logger.log(highlight(JSON.stringify(config, null, 4), {language: 'json'}));
     }
     else if (format === 'js') {
-        logger.log(`export default ${JSON.stringify(config, null, 4)}`);
+        logger.log(highlight(`export default ${JSON.stringify(config, null, 4)}`, {language: 'javascript'}));
     }
     else {
-        logger.log(YAML.stringify(config, {
+        logger.log(highlight(YAML.stringify(config, {
             indent:         4,
             mapAsMap:       true,
             merge:          true,
             sortMapEntries: true,
-        }));
+        }), {language: 'yaml'}));
     }
 
     return 0;
